9cfa657c462d766217e0139b8711dd1cd5c6dfb6
stevendo <stevendo@me.com>
2020-06-11 15:49:24 -0400

    added checker for draw outcome

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -19,7 +19,7 @@ const onStartNewGame = function (event) {
 let currentMove = 'X' // Tracks current move
 let gamePiece         // Parameter for game move
 let gameIndex         // Parameter for game tile
-// let numTurns          // Turn count
+let numTurns = 0         // Turn count
 let gameWin = false   // Boolean if game has been won
 let gameModel = ['', '', '', '', '', '', '', '', '']    // Local game representation
 let winner = ''            // Variable to assign winner O or X
@@ -44,6 +44,8 @@ const onGameUpdate = function (event) {
     if (gameModel[0] !== '' && gameModel[0] === gameModel[1] && gameModel[0] === gameModel[2]) {
       gameWin = true
       winner = currentMove
+      // block unused tiles
+      $('.content').removeClass('free').addClass('taken')
 
       // Update API
       api.gameUpdate(gamePiece, gameIndex, gameWin)
@@ -52,6 +54,8 @@ const onGameUpdate = function (event) {
     } else if (gameModel[3] !== '' && gameModel[3] === gameModel[4] && gameModel[3] === gameModel[5]) {
       gameWin = true
       winner = currentMove
+      // block unused tiles
+      $('.content').removeClass('free').addClass('taken')
 
       // Update API
       api.gameUpdate(gamePiece, gameIndex, gameWin)
@@ -60,6 +64,8 @@ const onGameUpdate = function (event) {
     } else if (gameModel[6] !== '' && gameModel[6] === gameModel[7] && gameModel[6] === gameModel[8]) {
       gameWin = true
       winner = currentMove
+      // block unused tiles
+      $('.content').removeClass('free').addClass('taken')
 
       // Update API
       api.gameUpdate(gamePiece, gameIndex, gameWin)
@@ -68,6 +74,8 @@ const onGameUpdate = function (event) {
     } else if (gameModel[0] !== '' && gameModel[0] === gameModel[3] && gameModel[0] === gameModel[6]) {
       gameWin = true
       winner = currentMove
+      // block unused tiles
+      $('.content').removeClass('free').addClass('taken')
 
       // Update API
       api.gameUpdate(gamePiece, gameIndex, gameWin)
@@ -76,6 +84,8 @@ const onGameUpdate = function (event) {
     } else if (gameModel[1] !== '' && gameModel[1] === gameModel[4] && gameModel[1] === gameModel[7]) {
       gameWin = true
       winner = currentMove
+      // block unused tiles
+      $('.content').removeClass('free').addClass('taken')
 
       // Update API
       api.gameUpdate(gamePiece, gameIndex, gameWin)
@@ -84,6 +94,8 @@ const onGameUpdate = function (event) {
     } else if (gameModel[2] !== '' && gameModel[2] === gameModel[5] && gameModel[2] === gameModel[8]) {
       gameWin = true
       winner = currentMove
+      // block unused tiles
+      $('.content').removeClass('free').addClass('taken')
 
       // Update API
       api.gameUpdate(gamePiece, gameIndex, gameWin)
@@ -92,6 +104,8 @@ const onGameUpdate = function (event) {
     } else if (gameModel[0] !== '' && gameModel[0] === gameModel[4] && gameModel[0] === gameModel[8]) {
       gameWin = true
       winner = currentMove
+      // block unused tiles
+      $('.content').removeClass('free').addClass('taken')
 
       // Update API
       api.gameUpdate(gamePiece, gameIndex, gameWin)
@@ -100,6 +114,8 @@ const onGameUpdate = function (event) {
     } else if (gameModel[2] !== '' && gameModel[2] === gameModel[4] && gameModel[2] === gameModel[6]) {
       gameWin = true
       winner = currentMove
+      // block unused tiles
+      $('.content').removeClass('free').addClass('taken')
 
       // Update API
       api.gameUpdate(gamePiece, gameIndex, gameWin)
@@ -112,18 +128,23 @@ const onGameUpdate = function (event) {
       } else {
         currentMove = 'X'
       }
-
+      // Count number of turns
+      numTurns++
+      // If number of turns reaches 9, it's a draw
+      if (numTurns <= 8) {
       // Messaging for next move
-      $('#player-turn').text(currentMove + ' Is the next turn')
-      // Update API
-      api.gameUpdate(gamePiece, gameIndex, gameWin)
-        .then(ui.gameUpdateSuccess)
-        .catch(ui.gameUpdateFailure)
+        $('#player-turn').text(currentMove + ' Is the next turn')
+        // Update API
+        api.gameUpdate(gamePiece, gameIndex, gameWin)
+          .then(ui.gameUpdateSuccess)
+          .catch(ui.gameUpdateFailure)
+      } else {
+        $('#player-turn').text("It's a Draw!")
+      }
     }
     console.log(gameModel, 'game model')
     console.log(gameWin, 'game win')
 }
-
   // api.gameUpdate(gamePiece, gameIndex, gameWin)
   //   .then(ui.gameUpdateSuccess)
   //   .catch(ui.gameUpdateFailure)
